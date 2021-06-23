8ef22afea83f552ab75f09d6b8e440761e925428
lawalkeyd <lawalkeyd@gmail.com>
2021-06-23 08:53:39 +0100

    First Commit: created scraper app, celery

---
+++ scraper/models.py
@@ -0,0 +1,18 @@
+# from django.db import models
+
+# # Create your models here.
+# class Images(models.Model):
+#     images = models.CharField(max_length=120)
+
+# class ScrapedInfo(models.Model):
+#     url = models.CharField(max_length='60')
+#     title = models.CharField(max_length=20)
+#     images = models.ForeignKey(Images, on_delete=models.PROTECT)
+#     text = models.CharField()
+
+#     class Meta:
+#         verbose_name = 'Currency'
+#         verbose_name_plural = 'Currencies'
+
+#     def __str__(self):
+#         return self.pair
