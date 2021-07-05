94b27be52e636534fc3cba0618f975726c0fee34
lawalkeyd <lawalkeyd@gmail.com>
2021-07-06 00:26:02 +0100

    feat: Scraped Data Model Structure

--- scraper/models.py
+++ scraper/models.py
@@ -1,17 +1,28 @@
 from django.db import models
 
 # Create your models here.
-class Images(models.Model):
-    images = models.CharField(max_length=120)
 
 class ScrapedInfo(models.Model):
     url = models.CharField(max_length=60)
-    title = models.CharField(max_length=20)
-    images = models.ForeignKey(Images, on_delete=models.PROTECT, null=True, blank=True)
-    text = models.TextField()
+    title = models.CharField(max_length=160)
 
     class Meta:
         verbose_name = 'Scraped Info'
 
     def __str__(self):
         return self.title
+
+class ParentInfo(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children')
+    title = models.CharField(max_length=160)        
+
+class Images(models.Model):
+    info = models.ForeignKey(ParentInfo, on_delete=models.CASCADE, related_name='images')
+    images = models.CharField(max_length=120)
+
+class Headings(models.Model):        
+    info = models.ForeignKey(ParentInfo, on_delete=models.CASCADE, related_name='headings')
+    text = models.CharField(max_length=160)
+
+class Details(models.Model):
+    models.TextField(verbose_name='details')
