7857087cc811ab4b0a13b33aa01633d78470c784
Kayode Lawal <lawalkeyd@gmail.com>
2021-09-29 17:45:06 +0100

    converts request from celery task to class

--- scraper/tasks.py
+++ scraper/tasks.py
@@ -8,24 +8,42 @@ from django.http import HttpResponse
 from QuickSlides.settings import BASE_DIR
 
 
-def Check(bs, no, parent):
-    tag = "tag"
-    child = "child"
-    text = "text"
-    heads = bs.find_all('h' + str(no))
-    if heads != None or (no > 6):
-        for item in heads:
-            parent[child] = {}
-            parent[tag] = item.text
-            Check(item, parent[child], no + 1)
-    else:
-        # image = bs.parent.find('img')
-        # if image:
-        #         Images.objects.create(info=parent, images=image.get_text)
-        paragraph = bs.parent.find_all('p')
-        if paragraph:
-            parent[text] = paragraph[0].get_text
-    return parent        
+class Scrape_Site:
+    filename  = slide = site_url = None
+    parent = {}
+    booktitle = ''
+
+    def __init__(self, request):
+        self.site_url = request.data.get('site_url')
+        print('Scraping Site ..')
+        req = Request(self.site_url, headers=request.headers.__dict__)
+        html = urlopen(req).read()
+        bs = BeautifulSoup(html, 'html.parser')
+        self.booktitle = bs.find('title').text
+        self.create_powerpoint()
+        self.find_info(bs, 1, self.parent)
+
+    def create_powerpoint(self):
+        present_dir = BASE_DIR  / 'slides_folder' 
+        prs = Presentation(present_dir / 'Ion.pptx')
+        self.slide = prs
+
+    def find_info(bs, no):
+        tag = "tag"
+        child = "child"
+        text = "text"
+        heads = bs.find_all('h' + str(no))
+        for head in heads:
+            heading_headers = head.parent.find_all('h' + str(no+1))
+            if heading_headers != None and (no <= 6):
+                    self.create_section(head.text)
+                    self.find_info(head.parent, no + 1)
+            else:
+                paragraph = head.parent.find_all('p')
+                if paragraph:
+                    self.create_slide(head.text, paragraph[0].get_text)
+                    parent[text] = paragraph[0].get_text
+        return parent        
 
              
 
@@ -38,20 +56,14 @@ def web_scrape(url):
     bs = BeautifulSoup(html, 'html.parser')
     # get first 5 rows
     parent = {}
-    parent["title"] = bs.find('title').text
-    result = Check(bs, 1, parent)
+    parent["title"] = BookTitle = bs.find('title').text
+    result = Find_Info(bs, 1, parent)
 
 
     # create objects in database
     present_dir = BASE_DIR  / 'slides_folder' 
-   
     prs = Presentation(present_dir / 'Ion.pptx')
 
-    # first_slide = prs.slides[0]
-    # title = first_slide.shapes.title
-    # subtitle = first_slide.placeholders[1]
-    # title.text = result["title"]
-    # subtitle.text = "Subtitle"
 
     if "child" in result:
         slide = prs.slides[0]
@@ -60,7 +72,7 @@ def web_scrape(url):
 
     response = HttpResponse(content_type='application/vnd.ms-powerpoint')
     response['Content-Disposition'] = 'attachment; filename="sample.pptx"'
-    prs.save(present_dir / 'Complete.pptx')
+    prs.save(present_dir / '{}.pptx'.format(BookTitle))
     # ppt = source_stream.getvalue()
     # source_stream.close()
     # response.write(ppt)
